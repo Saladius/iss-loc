02c07e01e21d2a6972cc548f8f0f8bb96f520b38
f_fal <f_fal@MSI>
2024-04-03 14:57:45 +0000

    initial state of init spring project

---
+++ src/test/java/com/ff/issloc/IsslocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ff.issloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsslocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
